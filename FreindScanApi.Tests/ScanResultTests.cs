13f5d2dbae612e61c6dd42267ef6fc7d4a1f9199
Viktor Veis <viktorveis@users.noreply.github.com>
2015-02-08 23:29:38 -0800

    Added the ScanResult class

---
+++ FreindScanApi.Tests/ScanResultTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
+using FriendScan;
+using FluentAssertions;
+
+namespace FriendScanApi.Tests
+{
+    /// <summary>
+    /// Summary description for UrlCollectionTests
+    /// </summary>
+    [TestClass]
+    public class ScanResultTests
+    {
+        [TestMethod]
+        public void ScanResult_NullFriendName_ThrowsArgumentNullException()
+        {
+            Action a = () => new ScanResult(null, new List<string>().AsReadOnly());
+            a.ShouldThrow<ArgumentNullException>();
+        }
+
+        [TestMethod]
+        public void ScanResult_NullBlogUrls_ThrowsArgumentNullException()
+        {
+            Action a = () => new ScanResult("Friend Name", null);
+            a.ShouldThrow<ArgumentNullException>();
+        }
+    }
+}
---
+++ FreindScanApi/ScanResult.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FriendScan
+{
+    /// <summary>
+    /// Represents all information found online for a friend. The class is immutable.
+    /// </summary>
+    public class ScanResult
+    {
+        /// <summary>
+        /// The friend name that was scanned.
+        /// </summary>
+        public string FriendName { get; private set; }
+
+        /// <summary>
+        /// A collection of blog URLs for teh friend.
+        /// </summary>
+        public IReadOnlyCollection<string> BlogUrls { get; private set; }
+
+        public ScanResult(string friendName, IReadOnlyCollection<string> blogUrls)
+        {
+            if (null == friendName) throw new ArgumentNullException("friendName");
+            if (null == blogUrls) throw new ArgumentNullException("blogUrls");
+
+            this.FriendName = friendName;
+            this.BlogUrls = blogUrls;
+        }
+    }
+}
